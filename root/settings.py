558ee118c5ca707a38ecc7e3cf7a65c716dd0f97
goldendevuz <goldendevuz@gmail.com>
2025-02-24 15:45:48 +0500

    15.45.24.02.2025

--- root/settings.py
+++ root/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
-from django.shortcuts import redirect
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,7 +24,7 @@ SECRET_KEY = 'django-insecure-73#ghp2rbny(=zen5jg+&9=3#9d5n1i*slqg)2cih=v#32j(90
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -318,3 +316,5 @@ JAZZMIN_UI_TWEAKS = {
         "success": "btn-outline-success"
     },
 }
+
+EAV2_PRIMARY_KEY_FIELD = "id"
